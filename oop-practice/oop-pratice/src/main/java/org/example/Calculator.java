d90cccbebdbad6e32321a8144f646b3ff9660e47
nodwon <nodwon5024@gmail.com>
2022-11-01 17:42:54 +0900

    init

---
+++ oop-practice/oop-pratice/src/main/java/org/example/Calculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+
+        return operand1+operand2;
+    }
+}
--- oop-practice/oop-pratice/src/test/java/org/example/WrongPasswordGenerator.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class WrongPasswordGenerator implements PasswordGeneratePolicy {
-    @Override
-    public String generatePassword() {
-        return "aabbccee";
-    }
-}
